b742350c2a09042af476a6bd6b35a84fd4f87356
marcinliwen <liwen.marcin@gmail.com>
2022-08-03 00:24:58 +0200

    DO configdatabase connecton

--- medusa-config.js
+++ medusa-config.js
@@ -29,8 +29,13 @@ const ADMIN_CORS = process.env.ADMIN_CORS || "http://localhost:7000,http://local
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 
 // Database URL (here we use a local database called medusa-development)
-const DATABASE_URL =
-  process.env.DATABASE_URL || "postgres://localhost/medusa-store";
+const DB_USERNAME = process.env.db.USERNAME || "";
+const DB_PASSWORD = process.env.db.PASSWORD || "";
+const DB_HOST = process.env.db.HOSTNAME || "";
+const DB_PORT = process.env.db.PORT || "";
+const DB_DATABASE = process.env.db.DATABASE || "";
+const DATABASE_URL = `postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+//process.env.DATABASE_URL || "postgres://localhost/medusa-store";
 
 // Medusa uses Redis, so this needs configuration as well
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
